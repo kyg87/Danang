0ba64958288c528131009d90f574760b7d3ff9b1
kyg87 <dridy@naver.com>
2018-03-25 16:43:44 +0900

    dd

--- client/src/app/services/board.service.ts
+++ client/src/app/services/board.service.ts
@@ -12,23 +12,23 @@ export class BoardService{
     }
 
     getBoards(page ,size){
-        return this.http.get('http://localhost:3000/api/boards?page='+page + '&size='+ size)
+        return this.http.get('http://125.129.60.150/api/boards?page='+page + '&size='+ size)
         .map(res=>res.json());
     }
 
     getBoard(id){
-        return this.http.get('http://localhost:3000/api/board/'+id).map(res=>res.json());
+        return this.http.get('http://125.129.60.150/api/board/'+id).map(res=>res.json());
     }
 
     addBoard(newBoard){
         var headers = new Headers();
 
         headers.append('Content-Type', 'application/json');
-        return this.http.post('http://localhost:3000/api/board',JSON.stringify(newBoard),{headers :headers}).map(res =>res.json());
+        return this.http.post('http://125.129.60.150/api/board',JSON.stringify(newBoard),{headers :headers}).map(res =>res.json());
     }
 
     deleteBoard(id){
-        return this.http.delete('http://localhost:3000/api/board/'+id).map(res=>res.json());
+        return this.http.delete('http://125.129.60.150/api/board/'+id).map(res=>res.json());
     }
 
     updateBoard(board) {
@@ -38,7 +38,7 @@ export class BoardService{
 
         headers.append('Content-Type', 'application/json');
 
-        return this.http.put('http://localhost:3000/api/board/' + board._id, JSON.stringify(board), { headers: headers }).map(res => res.json());
+        return this.http.put('http://125.129.60.150/api/board/' + board._id, JSON.stringify(board), { headers: headers }).map(res => res.json());
 
     }
 
@@ -46,12 +46,12 @@ export class BoardService{
         var headers = new Headers();
 
         headers.append('Content-Type', 'application/json');
-        return this.http.post('http://localhost:3000/api/reply',JSON.stringify(reply),{headers :headers}).map(res =>res.json());
+        return this.http.post('http://125.129.60.150/api/reply',JSON.stringify(reply),{headers :headers}).map(res =>res.json());
     }
 
     getReplys(id){
         console.log(id)
-        return this.http.get('http://localhost:3000/api/replys?id='+id)
+        return this.http.get('http://125.129.60.150/api/replys?id='+id)
         .map(res=>res.json());
     }
 
